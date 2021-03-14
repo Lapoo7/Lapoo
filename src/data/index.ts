8f0fb0c612bf55c2fd83ba3677bd891e49930558
wangwenliang <18635381860@163.com>
2021-03-14 20:22:38 +0800

    构建模板

---
+++ src/data/index.ts
@@ -0,0 +1,14 @@
+export const Navigates = [
+    {
+        $key: '$DOTNET',
+        class: ['item', 'net', 'active'],
+        label: '.NET',
+        catalog: [],
+    },
+    {
+        $key: '$NG',
+        class: ['item', 'angular'],
+        label: 'Angular',
+        catalog: [],
+    },
+];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,186 @@
 <template>
     <div class="lapoo">
-        <div class="profile"></div>
+        <div class="nav">
+            <div v-for="item of nav" :key="item.$key" :class="item.class" @click="onNavigate(item)">
+                {{ item.label }}
+            </div>
+        </div>
         <div class="wrapper">
-            <div class="list">
-                <div class="card">
-                    <div class="subject"></div>
-                    <div class="describe"></div>
-                </div>
-                <div class="card">
-                    <div class="subject"></div>
-                    <div class="describe"></div>
+            <div class="search"></div>
+            <div class="content">
+                <div class="item">
+                    <div class="image">
+                        <div class="describe">如何提升.NET的性能</div>
+                    </div>
+                    <div class="tags">
+                        <div class="tag">.NET</div>
+                    </div>
                 </div>
             </div>
         </div>
+        <router-view />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Navigates } from '@/data/index';
 
 @Component({
-    components: {
-        HelloWorld,
+    data: () => {
+        return {
+            nav: Navigates,
+        };
+    },
+    components: {},
+    methods: {
+        onNavigate(item) {
+            const actived = (this.$data.nav as any[]).find((m) => m.class.indexOf('active') > -1);
+            actived.class.splice(actived.class.indexOf('active'), 1);
+            item.class.push('active');
+        },
+        require(path) {
+            return require(path);
+        },
     },
 })
 export default class Home extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+* {
+    transition: all 0.5s;
+}
 .lapoo {
+    display: flex;
+    justify-content: center;
     width: 100%;
     height: 100%;
-    background-color: #f3f3f3;
-    display: flex;
-    .profile {
-        width: 400px;
-        height: 100%;
-        // #e1e4ff, #311080
-        background-image: linear-gradient(#8276a7, #061435);
-        border: none;
+    margin: 0 auto;
+    background-image: linear-gradient(to bottom right, #536aaf, #54a1e8);
+    padding: 10px;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+        'Helvetica Neue', sans-serif;
+    font-size: 14px;
+    .nav {
+        width: 200px;
+        height: 400px;
+        background-color: #fafafa2e;
+        border-radius: 3px;
+        padding: 5px;
+        box-sizing: border-box;
+        position: relative;
+        .item {
+            width: 100%;
+            height: 40px;
+            margin-top: 5px;
+            line-height: 40px;
+            padding-left: 5px;
+            box-sizing: border-box;
+            border-radius: 3px;
+            font-weight: 600;
+            color: #1e2e5d;
+            cursor: pointer;
+            outline: none;
+            background-color: #fafafab0;
+        }
+        .item:first-of-type {
+            margin-top: 0px;
+        }
+        .item:hover {
+            background-color: #fafafab5;
+            margin-left: -25px;
+            width: 215px;
+            transition-delay: 0.1s;
+        }
+        .item.active {
+            background-color: #fafafab5;
+            width: 188px;
+            margin-left: -40px;
+            width: 220px;
+        }
+        .item.active.net {
+            background-color: #4e2fd4;
+            color: #fff;
+        }
+        .item.active.angular {
+            background-color: #c14545;
+            color: #fff;
+        }
     }
     .wrapper {
-        width: calc(100% - 400px);
+        width: 600px;
         height: 100%;
-        background-color: #fff;
-        display: flex;
-        position: relative;
-        padding: 10px 0px;
-        box-sizing: border-box;
-        .list {
-            margin: 0 auto;
-            width: 800px;
-            height: 100%;
-            .card:first-of-type {
-                margin-top: 0px;
-            }
-            .card {
+        margin-left: 5px;
+        .search {
+            width: 100%;
+            height: 45px;
+            margin-bottom: 5px;
+            background-color: #fafafa2e;
+            border-radius: 3px;
+        }
+        .content {
+            width: 100%;
+            height: calc(100% - 50px);
+            background-color: #fafafa2e;
+            border-radius: 3px;
+            padding: 5px 5px 0px 5px;
+            box-sizing: border-box;
+            overflow-y: auto;
+            .item {
                 width: 100%;
-                height: 300px;
-                border: none;
-                border-radius: 5px;
-                background-color: #fafafa;
-                margin-top: 20px;
-                .subject {
+                height: 180px;
+                border-radius: 3px;
+                margin-bottom: 20px;
+                .image {
                     width: 100%;
-                    height: 220px;
-                    background-color: #ECECEC;
-                    border-radius: 5px 5px 0px 0px;
+                    height: calc(100% - 35px);
+                    background-color: rgb(219, 215, 215);
+                    position: relative;
+                    background-image: url('../static/images/demo.png');
+                    .describe {
+                        position: absolute;
+                        bottom: 0px;
+                        left: 0px;
+                        right: 0px;
+                        height: 40px;
+                        background-color: #4a4a4a80;
+                        color: #fff;
+                        font-weight: 600;
+                        font-size: 15px;
+                        line-height: 40px;
+                        padding: 0px 7px;
+                        letter-spacing: 1.5px;
+                    }
                 }
-                .describe {
-                    width: 100%;
-                    height: calc(100% - 220px);
-                    background-color: #f7f7f7;
-                    border-radius: 0px 0px 5px 5px;
+                .tags {
+                    height: 35px;
+                    line-height: 35px;
+                    display: flex;
+                    align-items: center;
+                    background: #fafafa47;
+                    padding: 0px 5px;
+                    .tag {
+                        width: fit-content;
+                        box-sizing: border-box;
+                        line-height: 0px;
+                        padding: 0px 10px;
+                        background-color: #4e2fd4;
+                        color: #fff;
+                        font-size: 12px;
+                        height: 25px;
+                        line-height: 25px;
+                        border-radius: 5px;
+                    }
                 }
             }
+            .item:last-of-type {
+                margin-bottom: 5px;
+            }
+        }
+        .content::-webkit-scrollbar {
+            width: 0px;
         }
     }
 }
